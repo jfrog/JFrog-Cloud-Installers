7c2c6a6dccd35f052c200dd54f0c0687fb5d015a
danielmkn <danielmi@jfrog.com>
2020-02-28 16:16:07 -0800

    Support for both RT6 and RT7 installations

--- AzureResourceManager/scripts/version_selector.sh
+++ AzureResourceManager/scripts/version_selector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $ARTIFACTORY_VERSION = 6.16.0 ] || [ $ARTIFACTORY_VERSION = 6.17.0 ] ; then
-  sh scripts/install_artifactory.sh
+  sh install_artifactory.sh
 else
-  sh scripts/install_artifactory7.sh
+  sh install_artifactory7.sh
 fi
