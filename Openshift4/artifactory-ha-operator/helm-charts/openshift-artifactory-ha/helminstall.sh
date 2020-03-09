c71db854a9b17d2be99f6ff42cff5b3ab8a7a2c3
John Peterson <johnp@jfrog.com>
2020-03-09 15:23:31 -0700

    updates to helm chart to subchart latest GA ha chart version 2.0.25 w/ redhat init container fix

--- Openshift4/artifactory-ha-operator/helm-charts/openshift-artifactory-ha/helminstall.sh
+++ Openshift4/artifactory-ha-operator/helm-charts/openshift-artifactory-ha/helminstall.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then echo "Skipping creation of persistent volume examples. Ensure there is available PVs 200Gi per node for HA."; else oc create -f pv-examples/; fi  
+if [[ -z "$1" ]]
+then 
+  echo "Skipping creation of persistent volume examples. Ensure there is available PVs 200Gi per node for HA." 
+else 
+  oc create -f pv-examples/ 
+  oc new-project jfrog-artifactory
+  oc create serviceaccount svcaccount -n jfrog-artifactory
+  oc adm policy add-scc-to-user privileged system:serviceaccount:jfrog-artifactory:svcaccount
+  oc adm policy add-scc-to-user anyuid system:serviceaccount:jfrog-artifactory:svcaccount
+  oc adm policy add-scc-to-group anyuid system:authenticated
 
-oc new-project jfrog-artifactory
-oc create serviceaccount svcaccount -n jfrog-artifactory
-oc adm policy add-scc-to-user privileged system:serviceaccount:jfrog-artifactory:svcaccount
-oc adm policy add-scc-to-user anyuid system:serviceaccount:jfrog-artifactory:svcaccount
-oc adm policy add-scc-to-group anyuid system:authenticated
+  # enables hostPath plugin for openshift system wide
+  oc create -f hostpathscc.yaml -n jfrog-artifactory
+  oc patch securitycontextconstraints.security.openshift.io/hostpath --type=merge --patch='{"allowHostDirVolumePlugin": true}'
+  oc adm policy add-scc-to-user hostpath system:serviceaccount:jfrog-artifactory:svcaccount
 
-# enables hostPath plugin for openshift system wide
-oc create -f scc.yaml -n jfrog-artifactory
-oc patch securitycontextconstraints.security.openshift.io/hostpath --type=merge --patch='{"allowHostDirVolumePlugin": true}' 
-oc adm policy add-scc-to-user hostpath system:serviceaccount:jfrog-artifactory:svcaccount
+  # create the license secret
+  oc create secret generic artifactory-license --from-file=artifactory.cluster.license
 
-# create the license secret
-oc create secret generic artifactory-license --from-file=./artifactory.cluster.license
+  # create the tls secret
+  oc create secret tls tls-ingress --cert=jfrog.team.crt --key=jfrog.team.key
+fi  
 
 # install via helm
-helm install . --generate-name \
-               --set artifactory.node.replicaCount=1 \
-               --set nginx.service.type=NodePort \
-               --set artifactory.license.secret=artifactory-license,artifactory.license.dataKey=artifactory.cluster.license
+helm install artifactory-ha . \
+               --set nginx.tlsSecretName=tls-ingress \
+               --set artifactory-ha.artifactory.node.replicaCount=1 \
+               --set artifactory-ha.artifactory.license.secret=artifactory-license,artifactory-ha.artifactory.license.dataKey=artifactory.cluster.license
