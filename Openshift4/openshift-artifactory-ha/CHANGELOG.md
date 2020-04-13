5234afa864bcd9d5b4a5ab3660310ece9b10e729
John Peterson <johnp@jfrog.com>
2020-04-13 10:37:19 -0700

    Openshift artifactory-ha helm chart changes for version 2.3.0 of jfrog/artifactory-ha chart

--- Openshift4/openshift-artifactory-ha/CHANGELOG.md
+++ Openshift4/openshift-artifactory-ha/CHANGELOG.md
@@ -1,5 +1,14 @@
 # JFrog  Openshift Artifactory-ha Chart Changelog
 All changes to this chart will be documented in this file.
 
-## [1.0.0] - March 09, 2020
+## [2.3.0] - April 13, 2020
+* Updating to latest jfrog/artifactory-ha helm chart version 2.3.0
+
+## [2.2.9] - April 11, 2020
+* Fixed issues with master key
+
+## [2.1.9] - March 17, 2020
+* Updated Artifactory version to 7.3.2
+
+## [2.0.35] - March 09, 2020
 * Updated Artifactory version to 7.2.1
--- Openshift4/openshift-artifactory-ha/helminstall.sh
+++ Openshift4/openshift-artifactory-ha/helminstall.sh
@@ -4,22 +4,14 @@ if [[ -z "$1" ]]
 then 
   echo "Skipping creation of persistent volume examples. Ensure there is available PVs 200Gi per node for HA." 
 else
-  oc new-project jfrog-artifactory
-  oc create serviceaccount svcaccount -n jfrog-artifactory
-  oc adm policy add-scc-to-user privileged system:serviceaccount:jfrog-artifactory:svcaccount
-  oc adm policy add-scc-to-user anyuid system:serviceaccount:jfrog-artifactory:svcaccount
-  oc adm policy add-scc-to-group anyuid system:authenticated
-
-  # enables hostPath plugin for openshift system wide
-  oc create -f hostpathscc.yaml -n jfrog-artifactory
-  oc patch securitycontextconstraints.security.openshift.io/hostpath --type=merge --patch='{"allowHostDirVolumePlugin": true}'
-  oc adm policy add-scc-to-user hostpath system:serviceaccount:jfrog-artifactory:svcaccount
+  # patch the restricted scc to allow the pods to run as anyuid
+  oc patch scc restricted --patch '{"fsGroup":{"type":"RunAsAny"},"runAsUser":{"type":"RunAsAny"},"seLinuxContext":{"type":"RunAsAny"}}' --type=merge
 
   # create the license secret
   oc create secret generic artifactory-license --from-file=artifactory.cluster.license
 
   # create the tls secret
-  oc create secret tls tls-ingress --cert=jfrog.team.crt --key=jfrog.team.key
+  oc create secret tls tls-ingress --cert=tls.crt --key=tls.key
 fi  
 
 # install via helm
