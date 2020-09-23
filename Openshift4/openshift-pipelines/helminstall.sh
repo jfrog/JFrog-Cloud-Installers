a0812bba472cb564ef2b9edcb97bd29a7ce003d3
John Peterson <johnp@jfrog.com>
2020-09-23 16:17:26 -0700

    Openshift pipelines working w/ bitnami rabbitmq need to refactor to jfrog rabbitmq

--- Openshift4/openshift-pipelines/helminstall.sh
+++ Openshift4/openshift-pipelines/helminstall.sh
@@ -19,8 +19,8 @@ helm upgrade --install pipelines . \
      --set pipelines.pipelines.joinKey=$JOIN_KEY \
      --set pipelines.pipelines.accessControlAllowOrigins_0=http://openshiftartifactoryha-nginx \
      --set pipelines.pipelines.accessControlAllowOrigins_1=http://openshiftartifactoryha-nginx \
-     --set pipelines.pipelines.msg.uiUser=guest \
-     --set pipelines.pipelines.msg.uiUserPassword=guest \
+     --set pipelines.pipelines.msg.uiUser=monitor \
+     --set pipelines.pipelines.msg.uiUserPassword=monitor \
      --set pipelines.postgresql.enabled=false \
      --set pipelines.global.postgresql.host=postgres-postgresql \
      --set pipelines.global.postgresql.port=5432 \
@@ -28,8 +28,8 @@ helm upgrade --install pipelines . \
      --set pipelines.global.postgresql.user=artifactory \
      --set pipelines.global.postgresql.password=password \
      --set pipelines.global.postgresql.ssl=false \
-     --set pipelines.rabbitmq.rabbitmq.username=monitor \
-     --set pipelines.rabbitmq.rabbitmq.password=monitor \
+     --set pipelines.rabbitmq.rabbitmq.username=user \
+     --set pipelines.rabbitmq.rabbitmq.password=bitnami \
      --set pipelines.rabbitmq.externalUrl=amqps://pipelines-rabbit.jfrog.tech \
      --set pipelines.pipelines.api.externalUrl=http://pipelines-api.jfrog.tech \
      --set pipelines.pipelines.www.externalUrl=http://pipelines-www.jfrog.tech
