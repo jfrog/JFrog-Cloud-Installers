e99814b3447d16cd3afac591f8a112306c35d8bb
John Peterson <johnp@jfrog.com>
2020-09-23 11:47:51 -0700

    Openshift pipelines work in progress

---
+++ Openshift4/openshift-pipelines/CHANGELOG.md
@@ -0,0 +1,5 @@
+# JFrog  Openshift Pipelines Chart Changelog
+All changes to this chart will be documented in this file.
+
+## [1.4.5] Sept 21, 2020
+* Adding Openshift Pipelines helm chart version 1.4.5 app version 1.7.2
---
+++ Openshift4/openshift-pipelines/helminstall.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo "Installing Pipelines"
+
+if [ -z "$MASTER_KEY" ]
+then
+  MASTER_KEY=FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+fi
+
+if [ -z "$JOIN_KEY" ]
+then
+  JOIN_KEY=EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+fi
+
+helm upgrade --install pipelines . \
+     --set pipelines.pipelines.jfrogUrl=http://openshiftartifactoryha-nginx \
+     --set pipelines.pipelines.jfrogUrlUI=http://openshiftartifactoryha-nginx \
+     --set pipelines.pipelines.masterKey=$MASTER_KEY \
+     --set pipelines.pipelines.joinKey=$JOIN_KEY \
+     --set pipelines.pipelines.accessControlAllowOrigins_0=http://openshiftartifactoryha-nginx \
+     --set pipelines.pipelines.accessControlAllowOrigins_1=http://openshiftartifactoryha-nginx \
+     --set pipelines.pipelines.msg.uiUser=guest \
+     --set pipelines.pipelines.msg.uiUserPassword=guest \
+     --set pipelines.postgresql.enabled=false \
+     --set pipelines.global.postgresql.host=postgres-postgresql \
+     --set pipelines.global.postgresql.port=5432 \
+     --set pipelines.global.postgresql.database=pipelinesdb \
+     --set pipelines.global.postgresql.user=artifactory \
+     --set pipelines.global.postgresql.password=password \
+     --set pipelines.global.postgresql.ssl=false \
+     --set pipelines.rabbitmq.rabbitmq.username=monitor \
+     --set pipelines.rabbitmq.rabbitmq.password=monitor \
+     --set pipelines.rabbitmq.externalUrl=amqps://pipelines-rabbit.jfrog.tech \
+     --set pipelines.pipelines.api.externalUrl=http://pipelines-api.jfrog.tech \
+     --set pipelines.pipelines.www.externalUrl=http://pipelines-www.jfrog.tech
