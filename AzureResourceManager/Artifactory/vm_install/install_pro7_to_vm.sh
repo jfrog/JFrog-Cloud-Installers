af4fb3bbb3df08e0b900fa2e35423500dfd3fa78
Anup Singh <64248163+anupteal@users.noreply.github.com>
2021-07-08 19:20:10 +0530

    Update install_pro7_to_vm.sh

--- AzureResourceManager/Artifactory/vm_install/install_pro7_to_vm.sh
+++ AzureResourceManager/Artifactory/vm_install/install_pro7_to_vm.sh
@@ -11,6 +11,7 @@ apt-get update
 apt-get -y install wget curl>> /tmp/install-curl.log 2>&1
 
 #Generate Self-Signed Cert
+#wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
 mkdir -p /etc/pki/tls/private/ /etc/pki/tls/certs/
 openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/pki/tls/private/example.key -out /etc/pki/tls/certs/example.pem -days 356 -subj "/C=US/ST=California/L=SantaClara/O=IT/CN=*.localhost"
 
@@ -18,7 +19,8 @@ openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/pki/tls/private/example.k
 #echo "deb https://jfrog.bintray.com/artifactory-pro-debs ${UBUNTU_CODENAME} main" | tee -a /etc/apt/sources.list
 #curl --retry 5 https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | apt-key add -
 wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
-echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs ${UBUNTU_CODENAME} main" | sudo tee -a /etc/apt/sources.list;
+echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs ${UBUNTU_CODENAME} main" | sudo tee -a /etc/apt/sources.list
+wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
 apt-get update
 apt-get -y install nginx>> /tmp/install-nginx.log 2>&1
 apt-get -y install jfrog-artifactory-pro=${ARTIFACTORY_VERSION} >> /tmp/install-artifactory.log 2>&1
