8d5ff0781906b1a46bd2d9a596513d74e214afcd
Daniel Miakotkin <55418063+danielmkn@users.noreply.github.com>
2021-10-19 18:21:11 -0700

    ARM template. Use valid URLs for DB drivers in the vm install script (#171)

--- AzureResourceManager/Artifactory/vm_install/install_pro7_to_vm.sh
+++ AzureResourceManager/Artifactory/vm_install/install_pro7_to_vm.sh
@@ -16,9 +16,6 @@ mkdir -p /etc/pki/tls/private/ /etc/pki/tls/certs/
 openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/pki/tls/private/example.key -out /etc/pki/tls/certs/example.pem -days 356 -subj "/C=US/ST=California/L=SantaClara/O=IT/CN=*.localhost"
 
 # install the Artifactory PRO and Nginx
-#echo "deb https://jfrog.bintray.com/artifactory-pro-debs ${UBUNTU_CODENAME} main" | tee -a /etc/apt/sources.list
-#curl --retry 5 https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | apt-key add -
-
 echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs ${UBUNTU_CODENAME} main" |  tee -a /etc/apt/sources.list
 curl --retry 5 https://releases.jfrog.io/artifactory/api/gpg/key/public |  apt-key add -
 apt-get update
@@ -39,8 +36,8 @@ cat <<EOF >/var/opt/jfrog/artifactory/etc/info/installer-info.json
 EOF
 
 #Install database drivers (for Java 11, path is different for RT6 and RT7)
-curl --retry 5 -L -o /opt/jfrog/artifactory/app/artifactory/tomcat/lib/mysql-connector-java-5.1.38.jar https://bintray.com/artifact/download/bintray/jcenter/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar >> /tmp/install-databse-driver.log 2>&1
-curl --retry 5 -L -o /opt/jfrog/artifactory/app/artifactory/tomcat/lib/mssql-jdbc-7.4.1.jre11.jar https://bintray.com/artifact/download/bintray/jcenter/com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre11/mssql-jdbc-7.4.1.jre11.jar >> /tmp/install-databse-driver.log 2>&1
+curl --retry 5 -L -o /opt/jfrog/artifactory/app/artifactory/tomcat/lib/mysql-connector-java-5.1.38.jar https://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar >> /tmp/install-databse-driver.log 2>&1
+curl --retry 5 -L -o /opt/jfrog/artifactory/app/artifactory/tomcat/lib/mssql-jdbc-7.4.1.jre11.jar https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre11/mssql-jdbc-7.4.1.jre11.jar >> /tmp/install-databse-driver.log 2>&1
 curl --retry 5 -L -o /opt/jfrog/artifactory/app/artifactory/tomcat/lib/postgresql-42.2.18.jar https://jdbc.postgresql.org/download/postgresql-42.2.18.jar >> /tmp/install-databse-driver.log 2>&1
 
 #Configuring nginx
