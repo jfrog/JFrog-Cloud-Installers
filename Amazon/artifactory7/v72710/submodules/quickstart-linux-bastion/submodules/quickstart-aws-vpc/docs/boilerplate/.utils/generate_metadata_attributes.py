b082e55f9808e656c85872dc8ba565be920f356f
Giridharan Ramasamy <46867712+giri-vsr@users.noreply.github.com>
2021-12-02 21:29:06 +0530

    Checking in code for rt 7.27.10 and xray 3.35.0 version

---
+++ Amazon/artifactory7/v72710/submodules/quickstart-linux-bastion/submodules/quickstart-aws-vpc/docs/boilerplate/.utils/generate_metadata_attributes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import io
+import cfnlint
+from pathlib import Path
+
+
+def get_cfn(filename):
+    _decoded = cfnlint.decode.decode(filename, False)[0]
+    return _decoded
+
+def fetch_metadata():
+    metadata_attributes = set()
+    for yaml_cfn_file in Path('./templates').glob('*.template*'):
+        template = get_cfn(Path(yaml_cfn_file))
+        if not template:
+            raise Exception(f"cfn-lint failed to load {yaml_cfn_file} without errors. Failure")
+        _resources = template['Resources']
+        for _resource in _resources.values():
+            _type = _resource['Type'].lower()
+            metadata_attributes.add(_type.split('::')[1])
+            metadata_attributes.add(_type.replace('::','_'))
+    with open('docs/generated/services/metadata.adoc', 'w') as f:
+        f.write('\n')
+        for attr in sorted(metadata_attributes):
+            f.write(f":template_{attr}:\n")
+
+if __name__ == '__main__':
+    fetch_metadata()
